e0127c984d4e26b8f7fb501fd88197d6fdfe4c18
samarthagg <samarth_aggarwal@yahoo.co.in>
2018-08-17 13:08:17 +0530

    add linear

--- linear.py
+++ linear.py
@@ -1,45 +1,87 @@
-"""Custom topology example
+#!/usr/bin/python
 
-Two directly connected switches plus a host for each switch:
+"""
+   reference: https://www.youtube.com/watch?v=yHUNeyaQKWY
 
-   host --- switch --- switch --- host
+   mesh topology
 
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
+       s2------s3 ----h2
+       | *   * |
+       |   *   |
+       | *   * |
+ h1----s1-----s4------h0(c0) 
+
+ **** To prevent broadcast storm(by arp messages), 
+      enable spanning tree in each switch !!!!!
+
+      : in each switch, 
+         type "ovs-vsctl set bridge <bridge_name> stp-enable=true"
+   
 """
 
-from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
 
-class MyTopo( Topo ):
-    "Simple topology example."
+def emptyNet():
 
-    def __init__( self ):
-        "Create custom topo."
+    #######################################
+    # Constants
+    #######################################
 
-        # Initialize topology
-        Topo.__init__( self )
+    #######################################
+    # Run mininet
+    #######################################
+    net = Mininet( topo=None, build=False )
 
-        # Add hosts and switches
-        n=10;
-	host=[]
-	for i in range(n):
-		host.append(self.addHost('h'+str(i)))
-	switch=[]
-	for i in range(n):
-		switch.append(self.addSwitch('s'+str(i)))
-#	leftHost = self.addHost( 'h1' )
-#        rightHost = self.addHost( 'h2' )
-#        leftSwitch = self.addSwitch( 's3' )
-#        rightSwitch = self.addSwitch( 's4' )
+    info( '*** Adding controller\n' )
+    net.addController('c0', controller=RemoteController,ip="127.0.0.1",port=6633)
+    h0 = net.addHost('h0', ip='127.0.0.1')
 
-        # Add links
-	for i in range(n):
-		self.addLink( host[i], switch[i] )
-	for i in range(n-1):
-		self.addLink(switch[i],switch[i+1])
-#        self.addLink( leftHost, leftSwitch )
-#        self.addLink( leftSwitch, rightSwitch )
-#        self.addLink( rightSwitch, rightHost )
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.2.1' )
+    h2 = net.addHost( 'h2', ip='10.0.2.2' )
 
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1', cls=OVSSwitch )
+    s2 = net.addSwitch( 's2', cls=OVSSwitch )
+    s3 = net.addSwitch( 's3', cls=OVSSwitch )
+    # s4 = net.addSwitch( 's4', cls=OVSSwitch )
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+    info( '*** Creating links\n' )
+    ## controller - switch (s3)
+    net.addLink( h0, s3 )
+
+    ## host - switch
+    net.addLink( h1, s1 )
+    net.addLink( h2, s2 )
+
+    ## switches
+    switchList = (s1, s2, s3)
+    for index in range (0, len(switchList)-1):
+      # for index2 in range (index+1, len(switchList)):
+        net.addLink(switchList[index], switchList[index+1])
+
+    info( '*** Starting network\n')
+    net.start()
+
+    #info('*** Set ip address to switch\n')
+    s1.cmd('ifconfig switch1 10.0.2.1')
+    s2.cmd('ifconfig switch2 10.0.2.2')
+    s3.cmd('ifconfig switch3 10.0.2.3')
+
+    info('*** Enable spanning tree\n')
+    s1.cmd('ovs-vsctl set bridge switch1 stp-enable=true')
+    s2.cmd('ovs-vsctl set bridge switch2 stp-enable=true')
+    s3.cmd('ovs-vsctl set bridge switch3 stp-enable=true')
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    emptyNet()
--- ring.py
+++ ring.py
@@ -47,18 +47,18 @@ def emptyNet():
     s1 = net.addSwitch( 's1', cls=OVSSwitch )
     s2 = net.addSwitch( 's2', cls=OVSSwitch )
     s3 = net.addSwitch( 's3', cls=OVSSwitch )
-    # s4 = net.addSwitch( 's4', cls=OVSSwitch )
+    s4 = net.addSwitch( 's4', cls=OVSSwitch )
 
     info( '*** Creating links\n' )
-    ## controller - switch (s3)
-    net.addLink( h0, s3 )
+    ## controller - switch (s4)
+    net.addLink( h0, s4 )
 
     ## host - switch
     net.addLink( h1, s1 )
-    net.addLink( h2, s2 )
+    net.addLink( h2, s3 )
 
     ## switches
-    switchList = (s1, s2, s3)
+    switchList = (s1, s2, s3, s4)
     for index in range (0, len(switchList)):
       for index2 in range (index+1, len(switchList)):
         net.addLink(switchList[index], switchList[index2])
@@ -70,11 +70,13 @@ def emptyNet():
     s1.cmd('ifconfig s1 10.0.2.1')
     s2.cmd('ifconfig s2 10.0.2.2')
     s3.cmd('ifconfig s3 10.0.2.3')
+    s4.cmd('ifconfig s4 10.0.2.4')
 
     info('*** Enable spanning tree\n')
     s1.cmd('ovs-vsctl set bridge s1 stp-enable=true')
     s2.cmd('ovs-vsctl set bridge s2 stp-enable=true')
     s3.cmd('ovs-vsctl set bridge s3 stp-enable=true')
+    s4.cmd('ovs-vsctl set bridge s4 stp-enable=true')
 
     info( '*** Running CLI\n' )
     CLI( net )
